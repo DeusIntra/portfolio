9d8e29dbdb39a013c9d6674c3d25a3bf424dbc96
DeusIntra <deusintra@yahoo.com>
2025-09-18 22:15:07 +0500

    finishing touches for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'https://deusintra.github.io/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
